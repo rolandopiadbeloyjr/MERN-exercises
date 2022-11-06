0f1bc19a3dfc09f110fcb2b408450c4d4bb3b255
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-06 23:09:52 +0800

    Add files via upload

---
+++ 6-React DOM/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render( <h1>Hello React World</h1>,
+<App />,
+document.getElementById('root')
+);
