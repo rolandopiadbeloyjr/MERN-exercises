520eb5693dea57f76cea19683a777dac18551abd
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-13 03:04:34 +0800

    Update App.js

--- 11-Callback Handlers in JSX/App.js
+++ 11-Callback Handlers in JSX/App.js
@@ -5,7 +5,7 @@ const App = () => {
     {
       title: 'React',
       url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      author: ' Jordan Walke ',
       num_comments: 3,
       points: 4,
       objectID: 0,
@@ -13,7 +13,7 @@ const App = () => {
     {
       title: 'Redux',
       url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      author: ' Dan Abramov, Andrew Clark ',
       num_comments: 2,
       points: 5,
       objectID: 1,
