8817bb2da97932e461f3e0e27be76ee8736e09e0
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 23:05:50 +0800

    Update App.js

--- 8-Handler Function in JSX/App.js
+++ 8-Handler Function in JSX/App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const list = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+    author: ' Jordan Walke ',
     num_comments: 3,
     points: 4,
     objectID: 0,
@@ -12,7 +14,7 @@ const list = [
   {
     title: 'Redux',
     url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    author: ' Dan Abramov, Andrew Clark ',
     num_comments: 2,
     points: 5,
     objectID: 1,
@@ -20,7 +22,10 @@ const list = [
 ];
 
 const App = () => (
-  <div>
+  <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+<div>
     <h1>My Hacker Stories</h1>
 
     <Search />
@@ -29,6 +34,16 @@ const App = () => (
 
     <List />
   </div>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div>
 );
 
 const Search = () => {
@@ -60,3 +75,4 @@ const List = () => (
 );
 
 export default App;
+
