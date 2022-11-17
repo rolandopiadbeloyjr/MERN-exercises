ef6e408aed9ab994be672b2dd31f065f37bf771a
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 22:18:54 +0800

    Update App.js

--- 1-Meet the React Component/App.js
+++ 1-Meet the React Component/App.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const title = 'React';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <div>
       <h1>Hello World</h1>
     </div>
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+
+        >
+        Learn React
+        </a>
+        </header>
+      </div>
   );
 }
 
