b020aa7e1a8732e12f4cce07f0c7d1287bcb821b
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 22:44:34 +0800

    Update App.js

--- 5-React Component Instantiation/App.js
+++ 5-React Component Instantiation/App.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 const list = [
 {
 title: 'React',
 url: 'https://reactjs.org/',
-author: 'Jordan Walke',
+author: ' Jordan Walke ',
 num_comments: 3,
 points: 4,
 objectID: 0,
@@ -11,7 +14,7 @@ objectID: 0,
 {
 title: 'Redux',
 url: 'https://redux.js.org/',
-author: 'Dan Abramov, Andrew Clark',
+author: ' Dan Abramov, Andrew Clark ',
 num_comments: 2,
 points: 5,
 objectID: 1,
@@ -20,6 +23,9 @@ objectID: 1,
 
 function App() {
   return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
   <div>
   <h1>My Hacker Stories</h1>
   <label htmlFor="search">Search: </label>
@@ -28,6 +34,16 @@ function App() {
   {/* render the list here */}
   {/* and by the way: that's how you do comments in JSX */}
   </div>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div>
   );
   }
 
