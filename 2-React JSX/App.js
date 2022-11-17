0fffa3f75e28a80e1a9a1b9e09df106bb7273de7
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 22:21:17 +0800

    Update App.js

--- 2-React JSX/App.js
+++ 2-React JSX/App.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function getTitle(title) {
   return title;
@@ -6,11 +8,27 @@ function getTitle(title) {
 
 function App() {
   return (
-    <div>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+  <div>
       <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      </div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      </header>
     </div>
   );
 }
