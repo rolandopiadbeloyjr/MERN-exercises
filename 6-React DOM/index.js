fbeeaf19def87dd18a864056e650a4db4e1ad980
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 22:51:12 +0800

    Update index.js

--- 6-React DOM/index.js
+++ 6-React DOM/index.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 ReactDOM.render( <h1>Hello React World</h1>,
 <App />,
 document.getElementById('root')
 );
+export default App;
