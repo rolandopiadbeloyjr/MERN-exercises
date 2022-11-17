47e3036cf8c235208a91e77a743d7d1ea9df314c
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 23:01:18 +0800

    Update App.js

--- 9-React Props/App.js
+++ 9-React Props/App.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 
 const App = () => {
   const stories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      author: ' Jordan Walke ',
       num_comments: 3,
       points: 4,
       objectID: 0,
@@ -13,7 +16,7 @@ const App = () => {
     {
       title: 'Redux',
       url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      author: ' Dan Abramov, Andrew Clark ',
       num_comments: 2,
       points: 5,
       objectID: 1,
@@ -21,7 +24,10 @@ const App = () => {
   ];
 
   return (
-    <div>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+  <div>
       <h1>My Hacker Stories</h1>
 
       <Search />
@@ -30,6 +36,16 @@ const App = () => {
 
       <List list={stories} />
     </div>
+    <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div>
   );
 };
 
