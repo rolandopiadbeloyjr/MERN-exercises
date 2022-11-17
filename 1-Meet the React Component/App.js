f351b95b02111058f412ed6999ab60a9ce7621e7
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-17 22:32:45 +0800

    Update App.js

--- 1-Meet the React Component/App.js
+++ 1-Meet the React Component/App.js
@@ -9,9 +9,6 @@ function App() {
     <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
       <div>
       <h1>Hello World</h1>
     </div>
